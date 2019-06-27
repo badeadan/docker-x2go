349e49e1628f1ac56961f8317091d80058ce5605
Daniel Badea <daniel.badea@windriver.com>
2019-06-27 20:32:33 +0000

    switch to bash

--- container_init.sh
+++ container_init.sh
@@ -8,11 +8,9 @@ PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
 echo "root:$PASS" | chpasswd
 
-adduser --disabled-password --shell /usr/bin/zsh --uid 1000 --gecos "" $USER
+adduser --disabled-password --no-create-home --shell /bin/bash --uid ${ID} --gecos "" $USER
 adduser $USER sudo
-
 echo $USER":"$PASSWORD | chpasswd
 adduser $USER x2gouser
 
 touch /.container_init
-
