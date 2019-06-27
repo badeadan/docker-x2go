1f38aacb83e393e6e0a6cde5ee8d9cbeafadf547
Daniel Badea <daniel.badea@windriver.com>
2019-06-27 21:10:25 +0000

    fix container startup

--- container_init.sh
+++ container_init.sh
@@ -4,11 +4,12 @@ if [ -f /.container_init ]; then
 	exit 0
 fi
 
+set -x
 PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
 echo "root:$PASS" | chpasswd
 
-adduser --disabled-password --no-create-home --shell /bin/bash --uid ${ID} --gecos "" $USER
+adduser --disabled-password --no-create-home --shell /bin/bash --gecos "" $USER
 adduser $USER sudo
 echo $USER":"$PASSWORD | chpasswd
 adduser $USER x2gouser
